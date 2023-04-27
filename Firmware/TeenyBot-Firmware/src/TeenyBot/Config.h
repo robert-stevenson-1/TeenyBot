6db9afb67c128ae35567a7a073eb88e75fd5aeec
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2023-04-27 18:50:34 +0100

    Started Firmware Dev
     - Created and Setup Robot's config options
    - Firmware written to test the Motors and PWM value controls

---
+++ Firmware/TeenyBot-Firmware/src/TeenyBot/Config.h
@@ -0,0 +1,29 @@
+// Robot's Motor configuration
+/* CONNECTIONS:
+ *
+ * ESP32 D33 - Motor Driver PWM 1A Input
+ * ESP32 D32 - Motor Driver PWM 1B Input
+ * ESP32 D25 - Motor Driver PWM 2A Input
+ * ESP32 D26 - Motor Driver PWM 2B Input
+ * ESP32 GND - Motor Driver GND
+ */
+
+#define MOTOR_1A 32 // ORANGE wire, right motor
+#define MOTOR_1B 33 // YELLOW wire, right motor
+#define MOTOR_2A 26 // PURPLE wire, left motor
+#define MOTOR_2B 25 // BLUE wire, left motor
+#define MOTOR_1A_PWM_CHANNEL 0 // PWM Channel
+#define MOTOR_1B_PWM_CHANNEL 1 // PWM Channel
+#define MOTOR_2A_PWM_CHANNEL 2 // PWM Channel
+#define MOTOR_2B_PWM_CHANNEL 3 // PWM Channel
+
+#define MOTOR_ENCODER_1A 13 //right encoder
+#define MOTOR_ENCODER_1B 12 //right encoder
+#define MOTOR_ENCODER_2A 27 //left encoder
+#define MOTOR_ENCODER_2B 14 //left encoder
+
+#define MOTOR_GEARING     20
+#define MOTOR_ENCODER_MULT 14
+
+#define MOTOR_PWM_FREQ 30000
+#define MOTOR_PWM_RESOLUTION 8
--- Firmware/TeenyBot-Firmware/src/main.cpp
+++ Firmware/TeenyBot-Firmware/src/main.cpp
@@ -1,9 +1,176 @@
 #include <Arduino.h>
+#include "TeenyBot/Config.h"
 
-void setup() {
-  // put your setup code here, to run once:
+#define SERIAL Serial
+
+#define pwmWrite ledcWrite
+
+// Create the Controller's multiple tasks
+TaskHandle_t TASK_task1;
+TaskHandle_t TASK_task2;
+
+// current motor Direction
+bool motorCurDirR = false; // false->forward
+bool motorCurDirL = false; // false->forward
+// last motor interrupt time
+long motorLastTimeR = 0;
+long motorLastTimeL = 0;
+// motor encoder counts
+long motorCountL = 0;
+long motorCountR = 0;
+
+// motor speed values
+double motorCurrentRPMR = 0;
+double motorCurrentRPML = 0;
+
+void leftMotorInterrupt()
+{
+  motorCurDirL = digitalRead(MOTOR_ENCODER_2B);
+  // track the encoder counts with respect to the direction it's turning
+  motorCurDirL ? motorCountL-- : motorCountL++;
+
+  // calc the rpm
+  uint32_t currentTime = micros();
+  if (motorLastTimeL < currentTime)
+  {
+    // did not wrap around
+    double rev = currentTime - motorLastTimeL; // us
+    rev = 1.0 / rev;                           // rev per us
+    rev *= 1000000;                            // rev per sec
+    rev *= 60;                                 // rev per min
+    rev /= MOTOR_GEARING;                      // account for gear ratio
+    rev /= MOTOR_ENCODER_MULT;                 // account for multiple ticks per rotation
+    motorCurrentRPML = rev;
+  }
+  motorLastTimeL = currentTime;
+  // Serial.println("left interupt");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void rightMotorInterrupt()
+{
+  motorCurDirR = digitalRead(MOTOR_ENCODER_1B);
+  // track the encoder counts with respect to the direction it's turning
+  motorCurDirR ? motorCountR-- : motorCountR++;
+
+  // calc the rpm
+  uint32_t currentTime = micros();
+  if (motorLastTimeR < currentTime)
+  {
+    // did not wrap around
+    double rev = currentTime - motorLastTimeR; // us
+    rev = 1.0 / rev;                           // rev per us
+    rev *= 1000000;                            // rev per sec
+    rev *= 60;                                 // rev per min
+    rev /= MOTOR_GEARING;                      // account for gear ratio
+    rev /= MOTOR_ENCODER_MULT;                 // account for multiple ticks per rotation
+    motorCurrentRPMR = rev;
+  }
+  motorLastTimeR = currentTime;
+  // Serial.println("right interupt");
+}
+
+void initMotorEncoders()
+{
+  // set the motor direction state
+  motorCurDirR = false;
+  motorCurDirL = false;
+  // set the encoder read input pins
+  //  Right:
+  pinMode(MOTOR_ENCODER_1A, INPUT_PULLUP);
+  pinMode(MOTOR_ENCODER_1B, INPUT_PULLUP);
+  // left:
+  pinMode(MOTOR_ENCODER_2A, INPUT_PULLUP);
+  pinMode(MOTOR_ENCODER_2B, INPUT_PULLUP);
+
+  // attach the interrupts to an encoder pin
+  attachInterrupt(MOTOR_ENCODER_1A, rightMotorInterrupt, RISING);
+  attachInterrupt(MOTOR_ENCODER_2A, leftMotorInterrupt, RISING);
+}
+
+void task1(void *pvParameters)
+{
+  SERIAL.print("Task1 running on core: ");
+  SERIAL.println(xPortGetCoreID());
+  // run task code in infinite loop
+  for (;;)
+  {
+    SERIAL.print(motorCurrentRPML);
+    SERIAL.print(", ");
+    SERIAL.print(motorCurrentRPMR);
+    SERIAL.println();
+    
+    vTaskDelay(10);
+  }
+}
+
+void task2(void *pvParameters)
+{
+  delay(1000);
+  SERIAL.print("Task2 running on core: ");
+  SERIAL.println(xPortGetCoreID());
+  // run task code in infinite loop
+  for (;;)
+  {
+    for (int i = 0; i < 255; i++)
+    {
+      digitalWrite(MOTOR_1A, HIGH);
+      pwmWrite(MOTOR_1A_PWM_CHANNEL, i);
+      pwmWrite(MOTOR_2A_PWM_CHANNEL, i);
+      vTaskDelay(200);
+    }
+    for (int i = 254; i > 0; i--)
+    {
+      digitalWrite(MOTOR_1A, HIGH);
+      pwmWrite(MOTOR_1A_PWM_CHANNEL, i);
+      pwmWrite(MOTOR_2A_PWM_CHANNEL, i);
+      vTaskDelay(200);
+    }
+  }
+}
+
+void setup()
+{
+  // start the serial connection
+  SERIAL.begin(115200);
+
+  // initialise the the motor encoders
+  initMotorEncoders();
+
+  // set motor PWM output
+  ledcSetup(MOTOR_1A_PWM_CHANNEL, MOTOR_PWM_FREQ, MOTOR_PWM_RESOLUTION);
+  ledcSetup(MOTOR_1B_PWM_CHANNEL, MOTOR_PWM_FREQ, MOTOR_PWM_RESOLUTION);
+  ledcSetup(MOTOR_2A_PWM_CHANNEL, MOTOR_PWM_FREQ, MOTOR_PWM_RESOLUTION);
+  ledcSetup(MOTOR_2B_PWM_CHANNEL, MOTOR_PWM_FREQ, MOTOR_PWM_RESOLUTION);
+  ledcAttachPin(MOTOR_1A, MOTOR_1A_PWM_CHANNEL);
+  ledcAttachPin(MOTOR_1B, MOTOR_1B_PWM_CHANNEL);
+  ledcAttachPin(MOTOR_2A, MOTOR_2A_PWM_CHANNEL);
+  ledcAttachPin(MOTOR_2B, MOTOR_2B_PWM_CHANNEL);
+
+  // =================================================================
+  // Setup the multiple tasks:
+  // =================================================================
+  // core 0 is in charge of Wifi and BT communication so avoid overloading the core.
+  xTaskCreatePinnedToCore(
+      task1,       /* Task function. */
+      "task1",     /* name of task. */
+      10000,       /* Stack size of task */
+      NULL,        /* parameter of the task */
+      1,           /* priority of the task */
+      &TASK_task1, /* Task handle to keep track of created task */
+      0            /* pin task to core (Core 0)*/
+  );
+
+  xTaskCreatePinnedToCore(
+      task2,       /* Task function. */
+      "task2",     /* name of task. */
+      10000,       /* Stack size of task */
+      NULL,        /* parameter of the task */
+      1,           /* priority of the task */
+      &TASK_task2, /* Task handle to keep track of created task */
+      1            /* pin task to core (Core 1)*/
+  );
+}
+
+void loop()
+{ /*PUT NOT CODE HERE*/
 }
