82a494b68cd43cde978d2cf89ac34794c7a46897
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2023-06-29 15:29:28 +0100

    added comparison function

--- Controller Firmware/src/esp_controller/Controller_Data.h
+++ Controller Firmware/src/esp_controller/Controller_Data.h
@@ -12,4 +12,21 @@ struct ControllerData {
     bool button3;
 };
 
+
+/// @brief Compare two ControllerData struct to see if they have any differences in the data they contain
+/// @param a Struct 'a' to compare against 'b'
+/// @param b Struct 'b' to compare against 'a'
+/// @return Returns true if no difference is found, else false as the data inside is different 
+bool compareControllerData(ControllerData a, ControllerData b){
+    if(a.x != b.x || a.y != a.y)
+        return false;
+    if(a.button1 != b.button1)
+        return false;
+    if(a.button2 != b.button2)
+        return false;
+    if(a.button3 != b.button3)
+        return false;
+    return true;
+}
+
 #endif
