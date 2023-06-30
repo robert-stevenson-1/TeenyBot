f1985c29b0698387fbd7d3fe7467fdc7c3b15305
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2023-06-30 08:37:23 +0100

    Updated Controller Data in Robot Firmware

--- TeenyBot Firmware/src/Controller/Controller_Data.h
+++ TeenyBot Firmware/src/Controller/Controller_Data.h
@@ -5,11 +5,36 @@
 
 // Struct containing the Controller state information
 struct ControllerData {
+    uint8_t maxSpeed;
+    uint8_t curSpeed;
     int x;
     int y;
     bool button1;
     bool button2;
     bool button3;
+    // Connected to the robot?
+    bool connected;
+    uint8_t failedCount;
+    // char macAddr[18]; //Eg.0C:B8:15:F8:C2:68
 };
+
+
+/// @brief Compare two ControllerData struct to see if they have any differences in the data they contain
+/// @param a Struct 'a' to compare against 'b'
+/// @param b Struct 'b' to compare against 'a'
+/// @return Returns true if no difference is found, else false as the data inside is different 
+inline bool compareControllerData(ControllerData a, ControllerData b){
+    if(
+        a.x != b.x ||
+        a.y != b.y ||
+        a.button1 != b.button1 ||
+        a.button2 != b.button2 ||
+        a.button3 != b.button3 ||
+        a.connected != b.connected)
+        if (a.connected != b.connected)
+            Serial.println("Connected State changed");
+        return false;
+    return true;
+}
 
 #endif
